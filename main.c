c0f85e0046b701a58b30bd8d0fb4aee17a884505
Dâmaso Júnio <junio.pereirab@gmail.com>
2022-07-30 20:53:19 -0300

    Implementando botoes

--- main.c
+++ main.c
@@ -9,6 +9,9 @@
 #define YELLOW_2 21
 #define RED_2 22
 
+#define BUTTON_1 12
+#define BUTTON_2 13
+
 #define true 1
 #define false 0
 typedef int bool;
@@ -30,8 +33,26 @@ struct Semaphore {
     light red;
 } typedef semaphore;
 
+struct State {
+    int s1;
+    int s2;
+    int s3;
+    int s4;
+    int s5;
+    int s6;
+    long time;
+    bool minimum;
+} typedef state;
+
 semaphore Primary;
 semaphore Secondary;
+
+bool minimumHasPassed = false;
+bool button1HasBeenPressed = false;
+bool button2HasBeenPressed = false;
+
+long lastInterruptTime = 0;
+int position = 0;
 
 light setupLight (int color, int pin) {
     light L;
@@ -64,25 +85,130 @@ void setState (int s1, int s2, int s3, int s4, int s5, int s6) {
     digitalWrite(Secondary.red.pin, s6);
 }
 
-void temporizador () {
-    setState(1, 0, 0, 0, 0, 1);
-    delay(17000);
-    setState(0, 1, 0, 0, 0, 1);
-    delay(3000);
-    setState(0, 0, 1, 1, 0, 0);
-    delay(7000);
-    setState(0, 0, 1, 0, 1, 0);
-    delay(3000);
+void timer (state S) {
+    setState(S.s1, S.s2, S.s3, S.s4, S.s5, S.s6);
+    if(S.minimum) {
+        delay(S.time / 2);
+        minimumHasPassed = true;
+        if (button1HasBeenPressed) {
+            position = 0;
+            minimumHasPassed = false;
+            button1HasBeenPressed = false;
+            return;
+        }
+
+        if (button2HasBeenPressed) {
+            position = 2;
+            minimumHasPassed = false;
+            button2HasBeenPressed = false;
+            return;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            delay((S.time / 2) / 10);
+            if (button1HasBeenPressed) {
+                position = 0;
+                minimumHasPassed = false;
+                button1HasBeenPressed = false;
+                return;
+            }
+
+            if (button2HasBeenPressed) {
+                position = 2;
+                minimumHasPassed = false;
+                button2HasBeenPressed = false;
+                return;
+            }
+        }
+    } else {
+        delay(S.time);
+    }
+
+    button1HasBeenPressed = false;
+    button2HasBeenPressed = false;
+    minimumHasPassed = false;
+    return;
+}
+
+void callback_button1 (void) {
+    long interruptTime = millis();
+    if (interruptTime - lastInterruptTime > 300){
+        button1HasBeenPressed = true;
+    }
+    lastInterruptTime = interruptTime;
+}
+
+void callback_button2 (void) {
+    long interruptTime = millis();
+    if (interruptTime - lastInterruptTime > 300){
+        button2HasBeenPressed = true;
+    }
+    lastInterruptTime = interruptTime;
 }
 
 int main () {
     printf("Iniciou\n");
     if(wiringPiSetup() == -1) return 1;
 
+    state states[4];
+
+    states[0].s1 = 1;
+    states[0].s2 = 0;
+    states[0].s3 = 0;
+    states[0].s4 = 0;
+    states[0].s5 = 0;
+    states[0].s6 = 1;
+
+    states[0].time = 20000;
+    states[0].minimum = true;
+
+    states[1].s1 = 0;
+    states[1].s2 = 1;
+    states[1].s3 = 0;
+    states[1].s4 = 0;
+    states[1].s5 = 0;
+    states[1].s6 = 1;
+
+    states[1].time = 3000;
+    states[1].minimum = false;
+    
+    states[2].s1 = 0;
+    states[2].s2 = 0;
+    states[2].s3 = 1;
+    states[2].s4 = 1;
+    states[2].s5 = 0;
+    states[2].s6 = 0;
+
+    states[2].time = 10000;
+    states[2].minimum = true;
+
+    states[3].s1 = 0;
+    states[3].s2 = 0;
+    states[3].s3 = 1;
+    states[3].s4 = 0;
+    states[3].s5 = 1;
+    states[3].s6 = 0;
+
+    states[3].time = 3000;
+    states[3].minimum = false;
+
     setupSemaphore();
   
+    pinMode(BUTTON_1, INPUT);
+    pinMode(BUTTON_2, INPUT);
+
+    pullUpDnControl(BUTTON_1, PUD_UP);
+    pullUpDnControl(BUTTON_2, PUD_UP);
+
+    wiringPiISR(BUTTON_1, INT_EDGE_RISING, &callback_button1);
+    wiringPiISR(BUTTON_2, INT_EDGE_RISING, &callback_button2);
+
     while(1) {
-        temporizador();
+        timer(states[position]);
+        position++;
+        if( position == 4 ) {
+            position = 0;
+        }
     }
 
     return 0;
